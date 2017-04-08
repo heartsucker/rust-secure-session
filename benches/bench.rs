c4ecdddba321e51e2029745e164795f3dcb74268
heartsucker <heartsucker@autistici.org>
2017-04-09 01:36:34 +0200

    fixed benchmarks

--- benches/bench.rs
+++ benches/bench.rs
@@ -8,90 +8,95 @@ extern crate test;
 
 use bincode::Infinite;
 use chrono::prelude::*;
-use secure_session::session::{Session, SessionTransport};
+use secure_session::session::Session;
+use std::collections::HashMap;
+use std::str;
 use test::Bencher;
 
 const STRINGS: &str = include_str!("./strings.txt");
 const KEY_32: [u8; 32] = *b"01234567012345670123456701234567";
 
-fn session_transport(x: usize) -> SessionTransport {
-    assert!(x <= 1000);
-
-    let mut session = Session::new();
+fn session_data(x: usize) -> Session<HashMap<String, String>> {
+    assert!(x <= 1000); // there are only 1000 lines in the file
+    let mut map = HashMap::new();
 
     for s in STRINGS.split("\n").take(x) {
-        let _ = session.insert_bytes(s, vec![0; 64]);
+        let _ = map.insert(s.to_string(), str::from_utf8(&[0; 64]).unwrap().to_string());
     }
 
     let expires = Some(UTC.ymd(2017, 1, 1).and_hms(0, 0, 0));
-    SessionTransport { expires: expires, session: session }
+    Session { expires: expires, value: Some(map) }
 }
 
 #[bench]
-fn session_transport_serialize_0_items(b: &mut Bencher) {
-    let transport = session_transport(0);
+fn session_data_serialize_0_items(b: &mut Bencher) {
+    let session = session_data(0);
     b.iter(|| {
-        let _ = bincode::serialize(&transport, Infinite).expect("failed to serialize");
+        let _ = bincode::serialize(&session, Infinite).expect("failed to serialize");
     });
 }
 
 #[bench]
-fn session_transport_deserialize_0_items(b: &mut Bencher) {
-    let transport = session_transport(0);
-    let bytes = bincode::serialize(&transport, Infinite).expect("failed to serialize");
+fn session_data_deserialize_0_items(b: &mut Bencher) {
+    let session = session_data(0);
+    let bytes = bincode::serialize(&session, Infinite).expect("failed to serialize");
     b.iter(|| {
-        let _: SessionTransport = bincode::deserialize(&bytes).expect("failed to deserialize");
+        let _: Session<HashMap<String, String>> = 
+            bincode::deserialize(&bytes).expect("failed to deserialize");
     });
 }
 
 #[bench]
-fn session_transport_serialize_10_items(b: &mut Bencher) {
-    let transport = session_transport(10);
+fn session_data_serialize_10_items(b: &mut Bencher) {
+    let session = session_data(10);
     b.iter(|| {
-        let _ = bincode::serialize(&transport, Infinite).expect("failed to serialize");
+        let _ = bincode::serialize(&session, Infinite).expect("failed to serialize");
     });
 }
 
 #[bench]
-fn session_transport_deserialize_10_items(b: &mut Bencher) {
-    let transport = session_transport(10);
-    let bytes = bincode::serialize(&transport, Infinite).expect("failed to serialize");
+fn session_data_deserialize_10_items(b: &mut Bencher) {
+    let session = session_data(10);
+    let bytes = bincode::serialize(&session, Infinite).expect("failed to serialize");
     b.iter(|| {
-        let _: SessionTransport = bincode::deserialize(&bytes).expect("failed to deserialize");
+        let _: Session<HashMap<String, String>> = 
+            bincode::deserialize(&bytes).expect("failed to deserialize");
     });
 }
 
 #[bench]
-fn session_transport_serialize_100_items(b: &mut Bencher) {
-    let transport = session_transport(100);
+fn session_data_serialize_100_items(b: &mut Bencher) {
+    let session = session_data(100);
     b.iter(|| {
-        let _ = bincode::serialize(&transport, Infinite).expect("failed to serialize");
+        let _ = bincode::serialize(&session, Infinite).expect("failed to serialize");
     });
 }
 
 #[bench]
-fn session_transport_deserialize_100_items(b: &mut Bencher) {
-    let transport = session_transport(100);
-    let bytes = bincode::serialize(&transport, Infinite).expect("failed to serialize");
+fn session_data_deserialize_100_items(b: &mut Bencher) {
+    let session = session_data(100);
+    let bytes = bincode::serialize(&session, Infinite).expect("failed to serialize");
     b.iter(|| {
-        let _: SessionTransport = bincode::deserialize(&bytes).expect("failed to deserialize");
+        let _: Session<HashMap<String, String>> = 
+            bincode::deserialize(&bytes).expect("failed to deserialize");
     });
 }
 
 #[bench]
-fn session_transport_serialize_1000_items(b: &mut Bencher) {
-    let transport = session_transport(1000);
+fn session_data_serialize_1000_items(b: &mut Bencher) {
+    let session = session_data(1000);
     b.iter(|| {
-        let _ = bincode::serialize(&transport, Infinite).expect("failed to serialize");
+        let _ = bincode::serialize(&session, Infinite).expect("failed to serialize");
     });
 }
 
 #[bench]
-fn session_transport_deserialize_1000_items(b: &mut Bencher) {
-    let transport = session_transport(1000);
-    let bytes = bincode::serialize(&transport, Infinite).expect("failed to serialize");
+fn session_data_deserialize_1000_items(b: &mut Bencher) {
+    let session = session_data(1000);
+    let bytes = bincode::serialize(&session, Infinite).expect("failed to serialize");
     b.iter(|| {
-        let _: SessionTransport = bincode::deserialize(&bytes).expect("failed to deserialize");
+        let _: Session<HashMap<String, String>> = 
+            bincode::deserialize(&bytes).expect("failed to deserialize");
     });
 }
 
@@ -99,80 +104,80 @@ macro_rules! benchmark {
     ($strct: ident, $md: ident) => {
         mod $md {
             use secure_session::session::{$strct, SessionManager};
-            use super::{session_transport, KEY_32};
+            use super::{session_data, KEY_32};
             use test::Bencher;
 
             #[bench]
-            fn session_transport_serialize_0_items(b: &mut Bencher) {
-                let transport = session_transport(0);
+            fn session_data_serialize_0_items(b: &mut Bencher) {
+                let session = session_data(0);
                 let manager = $strct::from_key(KEY_32);
                 b.iter(|| {
-                    let _ = manager.serialize(&transport).expect("failed to serialize");
+                    let _ = manager.serialize(&session).expect("failed to serialize");
                 });
             }
 
             #[bench]
-            fn session_transport_deserialize_0_items(b: &mut Bencher) {
-                let transport = session_transport(0);
+            fn session_data_deserialize_0_items(b: &mut Bencher) {
+                let session = session_data(0);
                 let manager = $strct::from_key(KEY_32);
-                let bytes = manager.serialize(&transport).expect("failed to serialize");
-                b.iter(|| {
-                    let _ = manager.deserialize(&bytes).expect("failed to deserialize");
-                });
-            }
-
-            #[bench]
-            fn session_transport_serialize_10_items(b: &mut Bencher) {
-                let transport = session_transport(10);
-                let manager = $strct::from_key(KEY_32);
-                b.iter(|| {
-                    let _ = manager.serialize(&transport).expect("failed to serialize");
-                });
-            }
-
-            #[bench]
-            fn session_transport_deserialize_10_items(b: &mut Bencher) {
-                let transport = session_transport(10);
-                let manager = $strct::from_key(KEY_32);
-                let bytes = manager.serialize(&transport).expect("failed to serialize");
+                let bytes = manager.serialize(&session).expect("failed to serialize");
                 b.iter(|| {
                     let _ = manager.deserialize(&bytes).expect("failed to deserialize");
                 });
             }
 
             #[bench]
-            fn session_transport_serialize_100_items(b: &mut Bencher) {
-                let transport = session_transport(100);
+            fn session_data_serialize_10_items(b: &mut Bencher) {
+                let session = session_data(10);
                 let manager = $strct::from_key(KEY_32);
                 b.iter(|| {
-                    let _ = manager.serialize(&transport).expect("failed to serialize");
+                    let _ = manager.serialize(&session).expect("failed to serialize");
                 });
             }
 
             #[bench]
-            fn session_transport_deserialize_100_items(b: &mut Bencher) {
-                let transport = session_transport(100);
+            fn session_data_deserialize_10_items(b: &mut Bencher) {
+                let session = session_data(10);
                 let manager = $strct::from_key(KEY_32);
-                let bytes = manager.serialize(&transport).expect("failed to serialize");
+                let bytes = manager.serialize(&session).expect("failed to serialize");
                 b.iter(|| {
                     let _ = manager.deserialize(&bytes).expect("failed to deserialize");
                 });
             }
 
             #[bench]
-            fn session_transport_serialize_1000_items(b: &mut Bencher) {
-                let transport = session_transport(1000);
+            fn session_data_serialize_100_items(b: &mut Bencher) {
+                let session = session_data(100);
                 let manager = $strct::from_key(KEY_32);
                 b.iter(|| {
-                    let _ = manager.serialize(&transport).expect("failed to serialize");
+                    let _ = manager.serialize(&session).expect("failed to serialize");
                 });
             }
 
             #[bench]
-            fn session_transport_deserialize_1000_items(b: &mut Bencher) {
-                let transport = session_transport(1000);
+            fn session_data_deserialize_100_items(b: &mut Bencher) {
+                let session = session_data(100);
                 let manager = $strct::from_key(KEY_32);
-                let bytes = manager.serialize(&transport).expect("failed to serialize");
+                let bytes = manager.serialize(&session).expect("failed to serialize");
+                b.iter(|| {
+                    let _ = manager.deserialize(&bytes).expect("failed to deserialize");
+                });
+            }
+
+            #[bench]
+            fn session_data_serialize_1000_items(b: &mut Bencher) {
+                let session = session_data(1000);
+                let manager = $strct::from_key(KEY_32);
+                b.iter(|| {
+                    let _ = manager.serialize(&session).expect("failed to serialize");
+                });
+            }
+
+            #[bench]
+            fn session_data_deserialize_1000_items(b: &mut Bencher) {
+                let session = session_data(1000);
+                let manager = $strct::from_key(KEY_32);
+                let bytes = manager.serialize(&session).expect("failed to serialize");
                 b.iter(|| {
                     let _ = manager.deserialize(&bytes).expect("failed to deserialize");
                 });
